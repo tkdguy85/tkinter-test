77f8559e8350b4bb24c21b120c500b7eda6f27e6
Dallas3 <dallasgordon3@gmail.com>
2026-01-26 18:13:34 -0800

    Test run of button stuffs

--- app-test.py
+++ app-test.py
@@ -6,11 +6,17 @@ root = tk.Tk()
 # Sets the title of the window
 root.title("Test App")
 
+def on_click():
+  # print("Button clicked")
+  label.config(text="Button Clicked")
+
 label = tk.Label(root, text="Button Label")
 label.grid(row=0, column=0) # Set the position of the label
 
+print(label.config().keys()) # Print all configuration options for the label
+
 # Add a button to the window
-btn = tk.Button(root, text="Push the Button")
+btn = tk.Button(root, text="Push the Button", command=on_click)
 btn.grid(row=0, column=1)
 
 root.mainloop()
